977c4bd6e6b806cbb513d4d3bfce207eb7986772
Keith <keith@vool.ie>
2016-04-09 21:15:42 +0100

    Replace file_get_contents with curl

--- data.php
+++ data.php
@@ -1,5 +1,16 @@
 <?php
+
+$ch = curl_init();
+
+curl_setopt($ch, CURLOPT_AUTOREFERER, TRUE);
+curl_setopt($ch, CURLOPT_HEADER, 0);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_URL, "http://waterlevel.ie/geojson/latest/");
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, TRUE);
+
+$data = curl_exec($ch);
+curl_close($ch);
+
 header('Content-type: application/json');
-$json = file_get_contents("http://waterlevel.ie/geojson/latest/");
-echo $json;
+echo $data;
 ?>
